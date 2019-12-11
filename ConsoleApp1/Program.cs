33534714a6f0993f48fd2f02b1687b616e0a66c8
ToBeNew <47148092+crebiz76@users.noreply.github.com>
2019-12-12 02:53:47 +0900

    Console Application 1-2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,13 @@ namespace ConsoleApp1
 	{
 		static void Main(string[] args)
 		{
+			string person;
+			if (args.Length > 0)	
+				person = args[0];
+			else
+				person = "AnyBody";
 			Console.WriteLine("Hello World!");
+			Console.WriteLine("Hello, {0}", person);
 			Console.ReadKey();
 		}
 	}
