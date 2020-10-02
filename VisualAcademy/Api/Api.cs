8dd6b668ff3e909042997554a7d02e1b7378f9f0
ToBeNew <crebiz76@gmail.com>
2020-10-03 00:55:04 +0900

    VisualAcademy study chapter 21

---
+++ VisualAcademy/Api/Api.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Api
+{
+    static void Main(string[] args)
+    {
+        // System.Math Class
+        // Static Member
+        System.Console.WriteLine("\n<--System.Math class - Static Member");
+        System.Console.WriteLine(Math.PI);
+        System.Console.WriteLine(Math.Max(3,5));
+        System.Console.WriteLine(Math.Abs(-10));
+
+        // System.Random Class
+        // Instance Member
+        System.Console.WriteLine("\n<--System.Random class - Instance Member");
+        Random dice = new Random();
+        var dice2 = new Random();
+        System.Console.WriteLine($"Dice throw: {dice.Next(1,7)}");
+        System.Console.WriteLine($"Dice throw: {dice2.Next(1,7)}");
+    }
+}
