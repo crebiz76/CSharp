0d77e89e564fdc815737de90a47666b5f55f46a9
ToBeNew <crebiz76@gmail.com>
2020-10-02 23:49:20 +0900

    VisualAcademy study until chapter 11

---
+++ VisualAcademy/RelationalOperator/RelationalOperator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class RelationalOperator
+{
+    static void Main(string[] args)
+    {
+#if Debug
+        // Relational Operator
+        int first = 10;
+        int second = 5;
+
+        bool r = false;         
+        r = (first > second);    // True
+        System.Console.WriteLine(r);
+
+        r = (first <= second);  // False
+        System.Console.WriteLine(r);
+
+        r = (first == second);  // False
+        System.Console.WriteLine(r);
+
+        r = (first != second);  // True
+        System.Console.WriteLine(r);
+
+#else
+        // Logical Operator
+        int i = 3;
+        int j = 5;
+        bool r = false;
+
+        r = (i == 3) && (j !=3 );
+        System.Console.WriteLine(r);
+        
+        r = (i != 3) || (j ==3 );
+        System.Console.WriteLine(r);
+
+        r = (i >= 5);
+        System.Console.WriteLine(!r);        
+
+#endif
+    }
+}
