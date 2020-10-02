4d6eb2536c49dd915188e4c52593dae6227e92da
ToBeNew <crebiz76@gmail.com>
2020-10-03 01:18:09 +0900

    VisualAcademy study until chapter 14

---
+++ VisualAcademy/SwitchStatement/SwitchStatement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class SwitchStatement
+{   
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("정수를 입력하세요. ");
+        int ans = Convert.ToInt32(Console.ReadLine());
+        
+        switch (ans)
+        {
+            case 1:
+            case 2:
+            case 3:
+                System.Console.WriteLine($"You chose a {ans}");
+                break;
+                
+            default:
+                System.Console.WriteLine($"You chose a wrong input");
+                break;
+        }
+    }
+}
