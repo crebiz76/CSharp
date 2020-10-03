52350f048bcb3a79e8456a26bf74cd39589486f3
ToBeNew <crebiz76@gmail.com>
2020-10-03 13:46:35 +0900

    VisualAcademy study until chapter 19

---
+++ VisualAcademy/Function/Function.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Function
+{
+    class Program
+    {
+        static void ShowMessage(string msg)
+        {
+            System.Console.WriteLine(msg);
+        }
+
+        static void Sum(int first, int second)
+        {
+            int sum = first + second;
+            System.Console.WriteLine($"합계: {sum}");
+        }
+
+        static int SumReturn(int first, int second)
+        {
+            int sum = first + second;
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            //
+            ShowMessage("Hello");
+            ShowMessage("world");
+            ShowMessage("");
+
+            //
+            Sum(3,5);
+            int result = SumReturn(3,5);
+            System.Console.WriteLine(result);
+
+            // int first = Convert.ToInt32(args[0]);
+            // int second = Convert.ToInt32(args[1]);
+            // System.Console.WriteLine(first,second);
+        }
+    }
+}
---
+++ VisualAcademy/FunctionDemo/FunctionDemo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace FunctionDemo
+{
+    class Program
+    {
+        static void Hi()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void Main(string[] args)
+        {
+            Hi();
+            Show();
+            ShowMessage("Hello Programming");
+
+            int result = SquareFunction(2);
+            System.Console.WriteLine(result);
+
+            double summation = GetSumTwoNumber(3.0, 4.5);
+            System.Console.WriteLine(summation);
+
+            System.Console.WriteLine(MaxFunction(3,5));
+            System.Console.WriteLine(MinFunction(3,5));
+        }
+
+        // Show method(function)
+        static void Show()
+        {
+            Console.WriteLine("Hello C#!");
+        }
+
+        // Function Parameter
+        static void ShowMessage(string msg)
+        {
+            System.Console.WriteLine(msg);
+        }
+
+        // Function Return Value
+        static int SquareFunction(int x)
+        {
+            int r = x * x;
+            return r;
+        }
+
+        static double GetSumTwoNumber(double a, double b)
+        {
+            double sum = a + b;
+            return sum;
+        }
+
+        static int MaxFunction(int x, int y)
+        {
+            return (x > y)? x : y;
+        }
+
+        static int MinFunction(int x, int y)
+        {
+            if (x >= y) return y;
+            else return x;
+        }
+    }
+}
