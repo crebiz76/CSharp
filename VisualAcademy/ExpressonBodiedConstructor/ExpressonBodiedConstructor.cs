3424d0dfa633f313607afe0e4172020074f9f2ec
ToBeNew <crebiz76@gmail.com>
2020-10-16 00:52:57 +0900

    VisualAcademy study until chapter 35-1

---
+++ VisualAcademy/ExpressonBodiedConstructor/ExpressonBodiedConstructor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Pet
+{
+    public string Name {get; set;}
+
+    public Pet(string name)
+    {
+        Name = name;
+    }
+}
+
+class ExpressonBodiedConstructor
+{
+    static void Main(string[] args)
+    {
+        Pet pet = new Pet("야옹이");
+        System.Console.WriteLine(pet.Name);
+    }
+}
